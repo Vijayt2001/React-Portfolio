4cb5498c44646f238b5d8055c92669177ff4ada0
Vijayt2001 <62590408+Vijayt2001@users.noreply.github.com>
2023-12-22 23:34:18 -0800

    updated assets and header with resume button and pdf

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import CV from '../../assets/resume.pdf'
 
 const CTA = () => {
   return (
     <div className = 'cta'>
-        {/* <a href={CV} download className='btn'>Download CV</a> */}
+        <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Info </a>
         <a href="https://vtolnoorkar577b.myportfolio.com/" className='btn btn-photo'>Photography Portfolio</a>
+        <a href=""></a>
 
 
         
